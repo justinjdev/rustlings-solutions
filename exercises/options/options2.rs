0c7d439a1f8a767fd8d8260715c00c5a95e74e09
Justin Jones <hock3ydud3@gmail.com>
2023-07-30 13:21:42 -0500

    some solutions

---
+++ exercises/options/options2.rs
@@ -0,0 +1,34 @@
+// options2.rs
+// Execute `rustlings hint options2` or use the `hint` watch subcommand for a hint.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn simple_option() {
+        let target = "rustlings";
+        let optional_target = Some(target);
+
+        if let Some(word) = optional_target {
+            assert_eq!(word, target);
+        }
+    }
+
+    #[test]
+    fn layered_option() {
+        let range = 10;
+        let mut optional_integers: Vec<Option<i8>> = vec![None];
+
+        for i in 1..(range + 1) {
+            optional_integers.push(Some(i));
+        }
+
+        let mut cursor = range;
+
+        while let Some(Some(integer)) = optional_integers.pop() {
+            assert_eq!(integer, cursor);
+            cursor -= 1;
+        }
+
+        assert_eq!(cursor, 0);
+    }
+}
